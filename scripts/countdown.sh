09ff6724241bf9fc3f05610ebe8464b3b5ca363b
Sinless777 <disdainful777@gmail.comn>
2024-03-27 03:11:05 +0000

    removed kubedb and rancher from the repo

--- scripts/countdown.sh
+++ scripts/countdown.sh
@@ -22,7 +22,7 @@ countdown() {
     sleep 1
     ((remaining--))
   done
-  echo "Countdown complete!\nContinuing Script..."
+  echo "Countdown complete!  \nContinuing Script..."
 }
 
 countdown $duration
